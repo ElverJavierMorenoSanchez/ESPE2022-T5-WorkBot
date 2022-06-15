0da4e64b6edfcb065752db80acf4e13ad396c624
Valery Ivannova Naranjo Zambrano <vinaranjo1@espe.edu.ec>
2022-06-15 09:56:31 -0500

    upload

--- 06-Code/02ServiciosWeb/PanaderiaRestService/src/main/java/Model/User.java
+++ 06-Code/02ServiciosWeb/PanaderiaRestService/src/main/java/Model/User.java
@@ -6,8 +6,115 @@ package Model;
 
 /**
  *
- * @author edisonnaranjodemera
+ * @author evalerynaranjo
  */
 public class User {
+    private String name;
+    private String surname;
+    private String address;
+    private String city;
+    private String phone;
+    private String email;
+    private String username;
+    private String password;
+    private String fechaNacimiento;
+    private CreditCard creditCard;
+
+    public User(String name, String surname, String address, String city, String phone, String email, String username, String password, String fechaNacimiento) {
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+        this.city = city;
+        this.phone = phone;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    
+    public User (){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public CreditCard getCreditCard() {
+        return creditCard;
+    }
+
+    public void setCreditCard(CreditCard creditCard) {
+        this.creditCard = creditCard;
+    }
+    
     
 }
+
