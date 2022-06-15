e2110e3afa7134204e37b67dad03187fd1078a0e
SantQC30 <lsquintanilla@espe.edu.ec>
2022-06-15 14:41:13 -0500

    delete the code of InvoiceRosource

--- 06-Code/02ServiciosWeb/PanaderiaRestService/src/main/java/Rest/InvoiceResource.java
+++ 06-Code/02ServiciosWeb/PanaderiaRestService/src/main/java/Rest/InvoiceResource.java
@@ -1,14 +1,7 @@
+
 package Rest;
 
-import ConnectionDB.ConnectionMongoDB;
 import Model.Invoice;
-import Model.User;
-import com.mongodb.MongoException;
-import com.mongodb.client.FindIterable;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
-import com.mongodb.client.MongoDatabase;
-import java.util.ArrayList;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
@@ -18,7 +11,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.core.MediaType;
-import org.bson.Document;
 
 /**
  * REST Web Service
@@ -32,33 +24,23 @@ public class InvoiceResource {
     @Context
     private UriInfo context;
 
-    ConnectionMongoDB connectionMongoDB = new ConnectionMongoDB();
-    MongoDatabase mongoDatabase;
-    MongoCollection userCollection;
-    MongoCollection cardCollection;
-    ArrayList<User> userList;
-    FindIterable<Document> findIterableCreditCard;
-    MongoCursor<Document> mongoCursorCreditCard;
-    FindIterable<Document> findIterableUser;
-    MongoCursor<Document> mongoCursorUser;
-    User user;
-    Invoice invoice;
-    
+    /**
+     * Creates a new instance of InvoiceResource
+     */
     public InvoiceResource() {
     }
-       
+
+    /**
+     * Retrieves representation of an instance of Rest.InvoiceResource
+     * @return an instance of Model.Invoice
+     */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public ArrayList getJson() {
-        try {
-            getCollections();
-            userList = getInvoiceList();
-        } catch (MongoException e){
-            System.out.println("Error" + e);
-        }
-        return userList;
+    public Invoice getJson() {
+        //TODO return proper representation object
+        throw new UnsupportedOperationException();
     }
-    
+
     /**
      * PUT method for updating or creating an instance of InvoiceResource
      * @param content representation for the resource
@@ -66,13 +48,5 @@ public class InvoiceResource {
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
     public void putJson(Invoice content) {
-    }
-
-    private void getCollections() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    private ArrayList<User> getInvoiceList() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
