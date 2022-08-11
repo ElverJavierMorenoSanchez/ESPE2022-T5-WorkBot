96b67f7a77a3b28402817eca4232f15edc80fe33
Roberto Carlos Pallo Yautibug <85329633+RobertoPallotech@users.noreply.github.com>
2022-08-11 00:49:59 -0500

    update productAxios

---
+++ 06-Code/04FinalProject/src/util/productAxios.js
@@ -0,0 +1,64 @@
+
+import axios from "axios";
+const baseUrl = 'http://3.86.206.55:3017';
+
+/********************** 
+    Products Crud
+**********************/
+export async function GetProducts() {
+    const token = localStorage.getItem("token");
+    try {
+      const response = await axios.get(`${baseUrl}/products`, {
+        headers: {
+          "access-token": token,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  export async function postProducts(product) {
+    const token = localStorage.getItem("token");
+    try {
+      const response = await axios.post(`${baseUrl}/products`, {
+        headers: {
+          "access-token": token,
+        },
+        data: product,
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  export async function putProduct(id, product) {
+    const token = localStorage.getItem("token");
+    try {
+      const response = await axios.put(`${baseUrl}/products/${id}`, {
+        headers: {
+          "access-token": token,
+        },
+        data: product,
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  export async function deleteProducts(id) {
+    const token = localStorage.getItem("token");
+    try {
+      const response = await axios.delete(`${baseUrl}/products/${id}`, {
+        headers: {
+          "access-token": token,
+        },
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
