051976e2c4d3c34deff4627e459fd946e46d4723
Valery Ivannova Naranjo Zambrano <vinaranjo1@espe.edu.ec>
2022-08-11 00:51:16 -0500

    upload

---
+++ 06-Code/04FinalProject/src/util/userAxios.js
@@ -0,0 +1,26 @@
+export async function getUsers() {
+    try {
+      const users = await axios.get(`${baseUrl}/users`);
+      return users.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  export async function putUser(id, body) {
+    try {
+      const user = await axios.put(`${baseUrl}/users/${id}`, body);
+      return user.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  export async function deleteUser(id) {
+    try {
+      const user = await axios.delete(`${baseUrl}/users/${id}`);
+      return user.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
