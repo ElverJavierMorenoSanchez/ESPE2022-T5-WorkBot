b2b637dff84dae428dfa097f8dfa823276f1cd11
ElverJavierMorenoSanchez <ejmoreno6@espe.edu.ec>
2022-08-11 01:08:47 -0500

    Update files

--- 06-Code/04FinalProject/src/util/userAxios.js
+++ 06-Code/04FinalProject/src/util/userAxios.js
@@ -1,29 +1,26 @@
-import axios from "axios";
-const baseUrl = "http://3.86.206.55:3017";
-
 export async function getUsers() {
-    try {
-      const users = await axios.get(`${baseUrl}/users`);
-      return users.data;
-    } catch (error) {
-      console.log(error);
-    }
+  try {
+    const users = await axios.get(`${baseUrl}/users`);
+    return users.data;
+  } catch (error) {
+    console.log(error);
   }
-  
-  export async function putUser(id, body) {
-    try {
-      const user = await axios.put(`${baseUrl}/users/${id}`, body);
-      return user.data;
-    } catch (error) {
-      console.log(error);
-    }
+}
+
+export async function putUser(id, body) {
+  try {
+    const user = await axios.put(`${baseUrl}/users/${id}`, body);
+    return user.data;
+  } catch (error) {
+    console.log(error);
   }
-  
-  export async function deleteUser(id) {
-    try {
-      const user = await axios.delete(`${baseUrl}/users/${id}`);
-      return user.data;
-    } catch (error) {
-      console.log(error);
-    }
+}
+
+export async function deleteUser(id) {
+  try {
+    const user = await axios.delete(`${baseUrl}/users/${id}`);
+    return user.data;
+  } catch (error) {
+    console.log(error);
   }
+}
