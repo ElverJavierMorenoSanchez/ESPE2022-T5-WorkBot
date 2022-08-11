e34d8b8f07043c3ef8cff147572e10df415aeaa6
SantQC30 <lsquintanilla@espe.edu.ec>
2022-08-11 11:24:43 -0500

    create invoiceCrud and code invoicemodalcrud

---
+++ 06-Code/04FinalProject/src/views/InvoiceCrud/InvoiceCrud.js
@@ -0,0 +1,194 @@
+import { Box } from "@mui/system";
+import React, { useEffect, useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import IconButton from "@mui/material/IconButton";
+import ModeIcon from "@mui/icons-material/Mode";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { getInvoices, deleteInvoice } from "../util/invoiceAxios";
+import InvoiceModalForm from "./InvoiceModalForm";
+import Button from "@mui/material/Button";
+
+function InvoiceCrud() {
+  const [InvoiceCrud, setInvoiceCrud] = useState([]);
+  const [invoice, setInvoice] = useState({});
+  const [editable, setEditable] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    loadInvoiceCrud();
+  }, []);
+
+  const loadInvoiceCrud = async () => {
+    const _invoiceCrud = await getInvoices();
+    setInvoiceCrud(_invoiceCrud);
+  };
+
+  const handleEditable = (edit) => setEditable(edit);
+  const handleClose = () => setOpen(false);
+  const handleOpen = (params) => {
+    setInvoice(params);
+    setOpen(true);
+  };
+
+  const handleDelete = async (id) => {
+    const confirm = window.confirm(
+      "¿Está seguro que desea eliminar el elemento?"
+    );
+    if (confirm) {
+      const itemDelete = await deleteInvoice(id);
+      loadInvoiceCrud();
+      console.log(itemDelete);
+    }
+  };
+
+  const columns = [
+    {
+      field: "key",
+      headerName: "ID",
+      width: 230,
+    },
+    {
+      field: "username",
+      headerName: "Nombre",
+      width: 100,
+    },
+    { field: "quantity", headerName: "Catidad", width: 100 },
+    { field: "productId", headerName: "Producto", width: 100 },
+    { field: "detail", headerName: "Detalle", width: 300 },
+    { field: "priceUnit", headerName: "Precio Unitario", width: 150 },
+    { field: "total", headerName: "Total", width: 200 },
+    {
+      field: "options",
+      headerName: "Opciones",
+      width: 150,
+      renderCell: (params) => (
+        <>
+          <div
+            target="_blank"
+            rel="noreferrer"
+            onClick={() => {
+              handleEditable(true);
+              handleOpen(params.row);
+            }}
+          >
+            <IconButton
+              aria-label="delete"
+              sx={{
+                color: "primary.main",
+                "&:hover": {
+                  color: "#111",
+                },
+              }}
+            >
+              <ModeIcon
+                sx={{
+                  fontSize: "1em",
+                }}
+              />
+            </IconButton>
+          </div>
+
+          <div
+            onClick={() => {
+              handleDelete(params.row.key);
+              return;
+            }}
+          >
+            <IconButton
+              aria-label="delete"
+              sx={{
+                color: "primary.main",
+                "&:hover": {
+                  color: "#111",
+                },
+              }}
+            >
+              <DeleteIcon
+                sx={{
+                  fontSize: "1em",
+                }}
+              />
+            </IconButton>
+          </div>
+        </>
+      ),
+    },
+  ];
+
+  return (
+    <>
+      <div id="header"></div>
+      {open ? (
+        <InvoiceModalForm
+          handleClose={handleClose}
+          invoice={invoice}
+          editable={editable}
+          loadData={loadInvoiceCrud}
+        />
+      ) : (
+        <></>
+      )}
+      <Box
+        sx={{
+          justifyContent: "center",
+          alignItems: "center",
+          display: "flex",
+          flexDirection: "column",
+          padding: "20px",
+          background: "#fff",
+          marginTop: "50px",
+        }}
+      >
+        <Button
+          variant="contained"
+          color="success"
+          onClick={() => {
+            handleEditable(false);
+            handleOpen({});
+          }}
+          sx={{
+            marginBottom: "50px",
+          }}
+        >
+          Añadir Factura
+        </Button>
+        <Box
+          sx={{
+            width: "75%",
+            height: "600px",
+            justifyContent: "center",
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+            paddingLeft: "20px",
+            paddingRight: "20px",
+            background: "#fff",
+            borderRadius: "15px",
+            boxShadow: "1px 1px 20px #333",
+          }}
+        >
+          <h1>Facturas</h1>
+          <div style={{ height: "100%", width: "100%" }}>
+            <DataGrid
+              rows={InvoiceCrud.map((item) => ({
+                key: item._id,
+                id: item.id,
+                username: item.username,
+                quantity: item.quantity,
+                productId: item.productId,
+                detail: item.detail,
+                priceUnit: item.priceUnit,
+                total: item.total,
+              }))}
+              columns={columns}
+              pageSize={10}
+              rowsPerPageOptions={[10]}
+            />
+          </div>
+        </Box>
+      </Box>
+    </>
+  );
+}
+
+export default InvoiceCrud;
---
+++ 06-Code/04FinalProject/src/views/InvoiceCrud/InvoiceModalForm.js
@@ -0,0 +1,218 @@
+import React, { useState, useEffect } from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+import TextField from "@mui/material/TextField";
+import { postInvoice, putInvoice } from "../util/invoiceAxios";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 500,
+  borderRadius: "15px",
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  align: "center",
+  p: 4,
+};
+
+export default function CreditCardModalForm(props) {
+  const [id, setId] = useState("");
+  const [username, setUsername] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [productId, setProductId] = useState("");
+  const [detail, setDetail] = useState("");
+  const [priceUnit, setPriceUnit] = useState("");
+  const [total, setTotal] = useState("");
+
+  useEffect(() => {
+    if (props.invoice) {
+      setId(props.invoice.id);
+      setUsername(props.invoice.username);
+      setQuantity(props.invoice.quantity);
+      setProductId(props.invoice.productId);
+      setDetail(props.invoice.detail);
+      setPriceUnit(props.invoice.priceUnit);
+      setTotal(props.invoice.total);
+    }
+  }, []);
+
+  const handleChangeId = (event) => {
+    setId(event.target.value);
+  };
+  const handleChangeUsername = (event) => {
+    setUsername(event.target.value);
+  };
+  const handleChangeQuantity = (event) => {
+    setQuantity(event.target.value);
+  };
+  const handleChangeProductId = (event) => {
+    setProductId(event.target.value);
+  };
+  const handleChangeDetail = (event) => {
+    setDetail(event.target.value);
+  };
+  const handleChangePriceUnit = (event) => {
+    setPriceUnit(event.target.value);
+  };
+  const handleChangeTotal = (event) => {
+    setTotal(event.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newInvoice = {
+      id,
+      username,
+      quantity,
+      productId,
+      detail,
+      priceUnit,
+      total,
+    };
+
+    if (props.editable) {
+      const invoice = await putInvoice(props.invoice.key, newInvoice);
+      console.log(invoice);
+      props.loadData();
+    } else {
+      const invoice = await postInvoice(newInvoice);
+      console.log(invoice);
+      props.loadData();
+    }
+
+    props.handleClose();
+  };
+
+  return (
+    <div>
+      <Modal
+        open={true}
+        onClose={props.handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <h2 align="center">{props.editable ? "Editar" : "Añadir"} Factura</h2>
+          <form onSubmit={handleSubmit}>
+            <TextField
+              fullWidth
+              id="id"
+              label="Id"
+              type="number"
+              value={id}
+              onChange={handleChangeId}
+              sx={{
+                marginTop: "15px",
+              }}
+            />
+            <TextField
+              fullWidth
+              id="username"
+              label="Nombre usuario"
+              type="text"
+              value={username}
+              onChange={handleChangeUsername}
+              sx={{
+                marginTop: "15px",
+              }}
+            />
+            <TextField
+              fullWidth
+              id="quantity"
+              label="Cantidad"
+              type="number"
+              value={quantity}
+              onChange={handleChangeQuantity}
+              sx={{
+                marginTop: "15px",
+              }}
+            />
+            <TextField
+              fullWidth
+              id="productId"
+              label="Id Producto"
+              type="number"
+              value={productId}
+              onChange={handleChangeProductId}
+              sx={{
+                marginTop: "15px",
+              }}
+            />
+            <TextField
+              fullWidth
+              id="detail"
+              label="Detalles"
+              type="text"
+              value={detail}
+              onChange={handleChangeDetail}
+              sx={{
+                marginTop: "15px",
+              }}
+            />
+            <TextField
+              fullWidth
+              id="priceUnit"
+              label="Precio Unitario"
+              type="number"
+              value={priceUnit}
+              onChange={handleChangePriceUnit}
+              sx={{
+                marginTop: "15px",
+              }}
+            />
+            <TextField
+              fullWidth
+              id="total"
+              label="Total"
+              type="number"
+              value={total}
+              onChange={handleChangeTotal}
+              sx={{
+                marginTop: "15px",
+              }}
+            />
+
+            <Box
+              sx={{
+                margin: "20px",
+                marginTop: "50px",
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "center",
+                gap: "20px",
+              }}
+            >
+              <Button
+                variant="contained"
+                color="success"
+                type="submit"
+                sx={{
+                  width: "100px",
+                }}
+                onClick={() => handleSubmit()}
+              >
+                Guardar
+              </Button>
+
+              <Button
+                variant="contained"
+                color="error"
+                onClick={props.handleClose}
+                sx={{
+                  width: "100px",
+                }}
+              >
+                Salir
+              </Button>
+            </Box>
+          </form>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
